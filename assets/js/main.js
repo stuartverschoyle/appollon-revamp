65cc5cc62fe03ee76c5988e7168cd6ea06d2db72
stuart verschoyle <stuart_verschoyle@hotmail.co.uk>
2021-07-03 09:43:35 +0100

    website amends

--- assets/js/main.js
+++ assets/js/main.js
@@ -256,6 +256,16 @@
     selector: '.doctor-lightbox'
   });  
 
+window.addEventListener('message', function(event) {
+  var frames = document.getElementsByTagName('iframe');
+  for (var i = 0; i < frames.length; i++) {
+      if (frames[i].contentWindow === event.source) {
+          jQuery(frames[i]).height(event.data);
+          break;
+      }
+  }
+});
+
   /**
    * Animation on scroll
    */
